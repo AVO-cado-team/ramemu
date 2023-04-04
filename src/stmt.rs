233f8e50cfbde11c43154bd5ade47c9310b44061
Ddystopia <alexanderbabak@proton.me>
2023-04-04 15:56:14 +0200

    Stmt should be copy

--- src/stmt.rs
+++ src/stmt.rs
@@ -61,7 +61,7 @@
 
 /// Represents a statement in the program, along with its line number from the source code.
 /// Statements are the basic building blocks of a program and define the operations to be performed.
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub enum Stmt {
   /// Loads value into register `0`
   Load(Value, usize),
@@ -119,7 +119,7 @@ impl Stmt {
 /// Examples:
 /// - `LOAD =5`: Loads the pure numeric value `5` into register 0.
 /// - `LOAD 5`: Loads the value stored in register 5 into register 0.
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub enum Value {
   /// Represents a pure numeric value, indicated by an equal sign (`=`) before the number.
   /// For example, `LOAD =5` loads the pure numeric value `5` into register 0.
