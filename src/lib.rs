faed396ca03423a50afe29965e23ebedcee47d4c
Oleksandr Babak <alexanderbabak@proton.me>
2023-06-30 16:32:21 +0200

    Refactor

--- src/lib.rs
+++ src/lib.rs
@@ -25,7 +25,27 @@
 
 #![warn(missing_docs)]
 
-pub mod errors;
+pub mod errors {
+    //! The [`errors`] module provides error types for various parsing and interpretation errors
+    //! that may occur during the parsing, validation, and execution of a program.
+    //!
+    //! This module includes the following error types:
+    //! - [`ParseError`] for parsing errors that may occur during parsing and validating input.
+    //! - [`InterpretError`] for interpretation errors that may occur during program execution.
+    //!
+    //! It also includes error-related types:
+    //! - [`InvalidArgument`] for representing various invalid argument errors.
+    //!
+    //! [`ParseError`]: enum.ParseError.html
+    //! [`InterpretError`]: enum.InterpretError.html
+    //! [`InvalidArgument`]: enum.InvalidArgument.html
+    //! [`errors`]: errors/index.html
+    mod parser;
+    mod ram;
+
+    pub use parser::*;
+    pub use ram::*;
+}
 pub mod parser;
 pub mod program;
 pub mod ram;
